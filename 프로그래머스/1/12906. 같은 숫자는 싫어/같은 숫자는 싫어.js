c75d6c7e6eeb7e94aae9545b94e3581979626f7f
Jo Chang Uk <sheepdog13@naver.com>
2024-04-08 17:25:34 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.32 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,8 +2,8 @@ function solution(arr)
 {
     var answer = [];
     for(let i = 0; i < arr.length; i++){
-        if(arr[i] !== arr[i+1]){
-            answer.push(arr[i])
+        if(arr[i-1] !== arr[i]){
+            answer.push(arr[i]);
         }
     }
     return answer;
