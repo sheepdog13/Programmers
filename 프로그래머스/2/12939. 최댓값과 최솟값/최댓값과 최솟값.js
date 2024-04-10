44c689ba1d33e89d5eff1124e6d1e1bb8b541606
Jo Chang Uk <sheepdog13@naver.com>
2024-04-10 10:43:40 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -2,15 +2,6 @@ function solution(s) {
     var answer = '';
     const input = s.toString().split(" ").map(v => +v);
     
-    let max = Number.MIN_SAFE_INTEGER;
-    let min = Number.MAX_SAFE_INTEGER;
-    
-    for(let i = 0; i < input.length; i++){
-        console.log(input[i]);
-        if(input[i] > max) max = input[i];
-        if(input[i] < min) min = input[i];
-    }
-    answer += min
-    answer += " " + max
+    answer += Math.min(...input) + " " + Math.max(...input)
     return answer;
 }
