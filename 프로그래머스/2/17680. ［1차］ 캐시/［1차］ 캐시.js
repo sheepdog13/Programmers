1f5faef7e80664b60c1dd0646633131e7befbdfb
Jo Chang Uk <sheepdog13@naver.com>
2024-04-29 12:37:03 +0900

    [level 2] Title: [1차] 캐시, Time: 3.51 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,28 +1,24 @@
 function solution(cacheSize, cities) {
     var answer = 0;
-    let cache = Array.from({length:cacheSize},()=>"");
-    console.log(cache)
-    cities.forEach(city => {
-        city = city.toLowerCase();
-        let hitpos = -1
-        for(let i = 0; i < cacheSize; i++){
-            if(cache[i] === city) hitpos = i;
-        }
-        if(hitpos === -1){
-            answer += 5;
-            for(let j = cacheSize-1; j >= 1; j--){
-                cache[j] = cache[j-1]
-            }            
-        }
-        else{
-            answer += 1;
-            for(let j = hitpos; j >= 1; j--){
-                
-                cache[j] = cache[j-1]
+    let cache = Array.from({ length: cacheSize }, (v) => (v = 0));
+    
+        for (let x of cities) {
+            x= x.toLowerCase()
+                let pos = -1;
+            
+            for(let i = 0; i < cacheSize; i++){
+                if(cache[i] === x) pos = i;
             }
+          if (pos !== -1) {
+             answer += 1
+            cache.splice(cache.indexOf(x), 1);
+            cache.unshift(x);
+          } else {
+             answer += 5
+            cache.pop();
+            cache.unshift(x);
+          }
         }
-                cache[0] = city;
-        
-    })
+
     return answer;
 }
