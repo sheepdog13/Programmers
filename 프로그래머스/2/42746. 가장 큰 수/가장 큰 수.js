14ee6826fab7f0daedaa2ab941475eb396c58c2f
Jo Chang Uk <sheepdog13@naver.com>
2024-05-01 15:36:44 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,9 +1,7 @@
 function solution(numbers) {
-    var answer = '';
-    let arr =[];
-    arr = numbers.map(v => v+"");
-    answer = arr.sort((a,b) => (b+a) - (a+b)).join("")
+    numbers = numbers.map((v) => ""+v);
+    
+    answer = numbers.sort((a,b)=> (b+a) - (a+b)).join("")
     
     return answer[0] === '0' ? '0' : answer;
-
 }
