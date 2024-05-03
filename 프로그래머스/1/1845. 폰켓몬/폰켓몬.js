94d247a5cf778186dadc8cf80e74b72f435ff5b1
Jo Chang Uk <sheepdog13@naver.com>
2024-05-03 11:55:47 +0900

    [level 1] Title: 폰켓몬, Time: 2.97 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,12 @@
 function solution(nums) {
     var answer = 0;
-    // 가져갈 수 있는 포켓몬 개수
-    const maxPokemonCount = nums.length/2
-    let pokemons = {}
-    for (let pokemon of nums) {
-        pokemons[pokemon] = true;
+    let monsters = new Map();
+    let n = nums.length;
+    for(let monster of nums){
+        if(monsters.has(monster)) monsters.set(monster, monsters.get(monster) + 1)
+        else monsters.set(monster, 1)
     }
-    // 포켓몬 종류수
-    const pokemons_num = Object.keys(pokemons).length;
-    // 가져갈 포켓몬수가 많을때
-    pokemons_num > maxPokemonCount ? answer = maxPokemonCount : answer = pokemons_num
+    
+    answer = (n/2) < monsters.size ? n/2 : monsters.size
     return answer;
 }
