a3a677e50cdb86735753cc925006820d4c374d00
Jo Chang Uk <sheepdog13@naver.com>
2024-05-10 14:34:56 +0900

    [Silver IV] Title: ZOAC 3, Time: 124 ms, Memory: 9620 KB -BaekjoonHub

--- 백준/Silver/20436. ZOAC 3/ZOAC 3.js
+++ 백준/Silver/20436. ZOAC 3/ZOAC 3.js
@@ -6,49 +6,36 @@ const left = input[0].split(" ")[0];
 const right = input[0].split(" ")[1];
 const words = input[1].split("");
 
-const leftBoard = [
-  ["q", "w", "e", "r", "t"],
-  ["a", "s", "d", "f", "g"],
-  ["z", "x", "c", "v"],
-];
-const rightBoard = [
-  ["", "y", "u", "i", "o", "p"],
-  ["", "h", "j", "k", "l"],
-  ["b", "n", "m"],
+const rights = "yuiophjklbnm";
+
+const keyBoard = [
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+  ["z", "x", "c", "v", "b", "n", "m"],
 ];
 
 function isRight(x) {
-  for (let i = 0; i < rightBoard.length; i++) {
-    for (let j = 0; j < rightBoard[i].length; j++) {
-      if (x === rightBoard[i][j]) return true;
-    }
+  for (let i = 0; i < rights.length; i++) {
+    if (x === rights[i]) return true;
   }
   return false;
 }
 
-function getIndex(x, isRight) {
-  if (isRight) {
-    for (let i = 0; i < rightBoard.length; i++) {
-      for (let j = 0; j < rightBoard[i].length; j++) {
-        if (x === rightBoard[i][j]) return (x = [i, j]);
-      }
-    }
-  } else {
-    for (let i = 0; i < leftBoard.length; i++) {
-      for (let j = 0; j < leftBoard[i].length; j++) {
-        if (x === leftBoard[i][j]) return (x = [i, j]);
-      }
+function getIndex(x) {
+  for (let i = 0; i < keyBoard.length; i++) {
+    for (let j = 0; j < keyBoard[i].length; j++) {
+      if (x === keyBoard[i][j]) return (x = [i, j]);
     }
   }
 }
 
 function solution(left, right, words) {
-  left = getIndex(left, false);
-  right = getIndex(right, true);
+  left = getIndex(left);
+  right = getIndex(right);
   let answer = 0;
   for (let x of words) {
     let position = isRight(x);
-    x = getIndex(x, position);
+    x = getIndex(x);
     if (position) {
       // 오른쪽
       answer += Math.abs(right[0] - x[0]) + Math.abs(right[1] - x[1]);
