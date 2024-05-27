6f324f6696819e36946cabac03fee973694dfe22
Jo Chang Uk <sheepdog13@naver.com>
2024-05-27 16:35:56 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 208 ms, Memory: 20220 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.js
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.js
@@ -2,21 +2,27 @@ var fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim();
 
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function solution(s) {
   let stack = [];
   let word = [];
   let words = [];
   for (let x of s) {
     if (stack.length) {
-      // 괄호안일때
+      // 괄호 안일 때
       stack.push(x);
       if (x === ">") {
         words.push(stack.join(""));
         stack = [];
       }
     } else {
+      // 괄호 밖일 때
       if (x === "<") {
         stack.push(x);
+        // 앞에서 단어를 저장 했을 경우
         if (word.length) {
           words.push(word.join(""));
           word = [];
@@ -26,6 +32,8 @@ function solution(s) {
       }
     }
   }
+
+  // ">"이 마지막이 아닐경우 남아있는 단어 push
   if (word.length) {
     words.push(word.join(""));
   }
@@ -33,12 +41,14 @@ function solution(s) {
   let result = "";
 
   for (let x of words) {
+    // 괄호일 경우 그대로 저장
     if (x[0] === "<") {
       result += x;
     } else {
+      // 괄호가 아닐 때 단어를 쪼갠 뒤 뒤집어서 단어 사이 뛰어서 저장
       result += x
         .split(" ")
-        .map((word) => word.split("").reverse().join(""))
+        .map((word) => reverseString(word))
         .join(" ");
     }
   }
